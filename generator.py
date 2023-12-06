27fc57667c201b11a9340d72824f60eef649ee83
yoones20 <yoonesakbari3@gmail.com>
2023-12-06 22:05:27 +0330

    finish 1

--- generator.py
+++ generator.py
@@ -1,2 +1,17 @@
 import time
 from random import randint
+
+def number_generate_with_delay(start,end):
+    
+    for num in range(start,end+1):
+       delay_seconds = int(input("delay seconds:"))
+       time.sleep(delay_seconds)
+       yield num
+start_num = 1
+
+end_num = 10
+
+Generator = number_generate_with_delay(start_num,end_num)
+
+for result in Generator:
+    print(result)
